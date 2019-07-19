9dcb16a940629d799e59f2181db0f0772f1d0224
xxdanbrowne <31517279+danb44545@users.noreply.github.com>
2019-07-18 20:13:30 -0600

    Update loginpage.php

--- loginpage.php
+++ loginpage.php
@@ -1,1 +1,14 @@
+<?php
 
+$username = $_POST["Username"];
+$pwd = $_POST["password"];
+
+if ($username=="ding" and $pwd=="dong")
+{
+  echo "successful login";
+  setcookie("xsession","true",time()+3600);
+}
+else
+{
+  header("Location: login1.html");
+}
