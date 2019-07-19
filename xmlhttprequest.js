194c08593782688c5be2a78791ce0ee6bd2e30e4
xxdanbrowne <31517279+danb44545@users.noreply.github.com>
2019-07-18 20:19:34 -0600

    Update xmlhttprequest.js

--- xmlhttprequest.js
+++ xmlhttprequest.js
@@ -1,1 +1,10 @@
-//tbd
+ <script>
+        var request = new XMLHttpRequest();    
+        request.onreadystatechange = function() {
+            if (request.readyState == 4 && request.status == 200) {
+                document.getElementById("result").innerHTML = request.responseText;
+            }
+        }
+        request.open("GET", "/xss.php?c=2", true);
+        request.send();
+    </script>
